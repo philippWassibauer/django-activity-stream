ff62b0bcc22c13a3891f497b05c3f20583d934c1
Philipp Wassibauer <phil@maptales.com>
2009-11-03 14:41:19 +0100

    new folder structure

--- activity-stream/admin.py
+++
@@ -1,6 +0,0 @@
-from activity_stream.models import * 
-from django.contrib import admin
-
-admin.site.register(ActivityFollower)
-admin.site.register(ActivityTypes)
-admin.site.register(ActivityStreamItem)
--- setup.py
+++ setup.py
@@ -3,10 +3,24 @@ from distutils.core import setup
 
 """ django-activity-stream instalation script """
 setup(
-    name = 'activity-stream',
+    name = 'activity_stream',
     description = 'generic activity feed system for users',
     author = 'Philipp Wassibauer',
     author_email = 'phil@maptales.com',
-    version = 0.1,
-    packages = ['activity-stream'],
-    )
+    url='http://github.com/philippWassibauer/django-activity-stream',
+    download_url='http://github.com/philippWassibauer/django-activity-stream/tarball/master',
+    license='MIT',
+    version = "0.1",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
+
