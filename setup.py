0d7bcf4e289e7614c7ee110a9e5f945b2aa247df
Philipp Wassibauer <phil@maptales.com>
2009-11-03 12:27:02 +0100

    fixing folder structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+""" django-activity-stream instalation script """
+setup(
+    name = 'activity-stream',
+    description = 'generic activity feed system for users',
+    author = 'Philipp Wassibauer',
+    author_email = 'phil@maptales.com',
+    version = 0.1,
+    packages = ['activity-stream'],
+    )
