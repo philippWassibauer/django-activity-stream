dc1e8eccf99dbee666a41cdf6e778b2d301b84ee
Philipp Wassibauer <phil@maptales.com>
2009-11-08 21:56:34 +0100

    bugfix in get_user_i_follow functions, returned wrong type

--- activity_stream/models.py
+++ activity_stream/models.py
@@ -127,13 +127,16 @@ def get_people_i_follow(user, count=None):
     if hasattr(settings, "ACTIVITY_GET_PEOPLE_I_FOLLOW"):
         return settings.ACTIVITY_GET_PEOPLE_I_FOLLOW(user)
     else:
-        return ActivityFollower.objects.filter(from_user=user).order_by('?').all()
+        followers =  ActivityFollower.objects.filter(from_user=user).order_by('?').all()
+        return [follower.to_user for follower in followers]
 
 def get_my_followers(user, count=None):
     if hasattr(settings, "ACTIVITY_GET_MY_FOLLOWERS"):
         return settings.ACTIVITY_GET_PEOPLE_I_FOLLOW(user)
     else:
-        return ActivityFollower.objects.filter(to_user=user).order_by('?').all()
+        followers = ActivityFollower.objects.filter(to_user=user).order_by('?').all()
+        return [follower.from_user for follower in followers]
+
 
 
 def create_activity_item(type, user, subject, data=None, safetylevel=1, custom_date=None):
