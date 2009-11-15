645490544f5f2e36e437922cbc34572b9f03688e
Philipp Wassibauer <phil@maptales.com>
2009-11-15 22:34:48 +0100

    fixing small bug in management.py

--- activity_stream/management.py
+++ activity_stream/management.py
@@ -15,12 +15,10 @@ def create_activity_types(app, created_models, verbosity, **kwargs):
         notification.create_notice_type("new_follower", _("New Follower"), _("somebody is following you now"), default=2)
 
     try:
-        ActivityTypes.objects.create(name="started_following", batch_time_minutes=30, is_batchable=True)
+        ActivityTypes.objects.get_or_create(name="started_following", batch_time_minutes=30, is_batchable=True)
+	ActivityTypes.objects.get_or_create(name="likes", is_batchable=False)
     except:
         pass
-    try:
-        ActivityTypes.objects.create(name="likes", is_batchable=False)
-    except:
-        pass
+    
 signals.post_syncdb.connect(create_activity_types, sender=activity_stream.models)
 
